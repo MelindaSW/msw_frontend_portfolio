40a1a2634d3b527c778a81fbd8169f62ecff3dbb
Melinda <sandstrommelinda@gmail.com>
2022-10-25 09:32:44 +0200

    change component names

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,6 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": ["error", { endOfLine: "auto" }],
-    "vue/multi-word-component-names": 0,
+    // "vue/multi-word-component-names": 0,
   },
 };
